32c89134df40b49b59b7df834d1bf0fe0c5fc40a
rivedroz23 <river.droz@gmail.com>
2019-05-30 08:10:30 -0700

    Bulk checkin

---
+++ js/four.js
@@ -0,0 +1,5 @@
+var friends = 'Moe,Larry,Curly,Jane,Emma,Elizabeth,Elinor,Mary,Darcy,Grey,Lydia,Harriet';
+
+var friendsarray = friends.split(","); 
+friendsarray.reverse();
+console.log(friendsarray);

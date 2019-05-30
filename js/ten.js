3052bc5bce0d26fd676e3afcadeaac4fb682ae25
Anthony Zardis <anthonyzardis@gmail.com>
2019-05-29 19:10:01 -0700

    Found the median

--- js/ten.js
+++ js/ten.js
@@ -3,9 +3,9 @@
 
 
 var ages = [83, 53, 37, 29, 60, 30, 66, 19, 59, 41, 9, 64, 19, 80, 24, 53, 70, 1, 53, 40, 92, 4, 71, 65, 8, 2, 51, 80, 94, 37, 80, 64, 19, 6, 14];
-var mid =  ages.sort().length;
-var nums = ages[17] + ages[16];
+ages.sort((a,b) => a - b);
+var midLow = Math.floor((ages.length - 1) / 2 );
+var midHi = Math.ceil((ages.length - 1) / 2 );
+var med = (ages[midLow] + ages[midHi]) / 2;
 
-console.log(mid);
-console.log(nums);
-//console.log();
+console.log(med);
